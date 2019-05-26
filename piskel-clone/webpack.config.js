0e461e6be11fce10820567d49034faedac4bcbb2
greatorangejuice <bypavelsnigirev@gmail.com>
2019-05-26 22:19:20 +0300

    feat: add html-plugin

--- piskel-clone/webpack.config.js
+++ piskel-clone/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/app.js',
@@ -23,4 +24,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
