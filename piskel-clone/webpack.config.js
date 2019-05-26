4d4cec38484c23c9a46be4dbc7fa89f7dd72b031
greatorangejuice <bypavelsnigirev@gmail.com>
2019-05-26 20:52:11 +0300

    initial enviroment

---
+++ piskel-clone/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { enforce: 'pre', test: /\.js$/, loader: 'eslint-loader' },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
