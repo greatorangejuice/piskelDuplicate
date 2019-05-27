c958193ea0da5a31f857a16bca0e34acaa736de1
greatorangejuice <bypavelsnigirev@gmail.com>
2019-05-27 17:41:20 +0300

    refactor: delete comment

--- piskel-clone/src/screens/canvas/index.js
+++ piskel-clone/src/screens/canvas/index.js
@@ -3,7 +3,6 @@ import Tools from '../../components/tools/toolsbar';
 export default class App {
   start() {
     const tools = new Tools();
-    // tools.checkToolsBar();
     const canvasTools = document.querySelector('.canvas-tools');
     canvasTools.addEventListener('click', tools.getActionButtons);
   }
