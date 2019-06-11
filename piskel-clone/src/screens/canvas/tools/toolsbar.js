962d519eae663195d35222219bf594abf38d54ac
greatorangejuice <bypavelsnigirev@gmail.com>
2019-06-11 14:25:16 +0300

    feat: new pixel pen

--- piskel-clone/src/screens/canvas/tools/toolsbar.js
+++ piskel-clone/src/screens/canvas/tools/toolsbar.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-vars */
-// import GetInitialCanvas from '../canvasField/initialcanvas';
-// import PixelArtDrawer from './pixelLine';
 /* eslint-disable prefer-destructuring */
 export default class Tools {
   setToolsbar() {
@@ -34,7 +31,7 @@ export default class Tools {
           // context.fillRect(newX, newY, 10, 10);
 
           // console.log(`newX: ${newX}, ceilX: ${Math.ceil(newX / 10) * 10}`);
-          context.fillRect(Math.ceil(newX / 10) * 10, Math.ceil(newY / 10) * 10, 10, 10);
+          context.fillRect(Math.ceil(newX / 4) * 4, Math.ceil(newY / 4) * 4, 4, 4);
 
           context.stroke();
           [x, y] = [newX, newY];
@@ -65,55 +62,24 @@ export default class Tools {
       const width = mycanvas.offsetWidth;
       console.log(width);
     };
-    // const save = () => {
-    //   // document.getElementById("canvasimg").style.border = "2px solid";
-    //   let dataURL = canvas.toDataURL();
-    //   document.getElementById("canvasimg").src = dataURL;
-    //   document.getElementById("canvasimg").style.display = "inline";
-    // }
 
-    // const getPixelPen = () => {
-    //   console.log(1);
-    // };
+    const brethPen = () => {
+      let startX = 0;
+      let startY = 0;
+      const finishX = 0;
+      const finishY = 0;
+      const isMouseDown = false;
 
-    // const goPen = () => {
-    //   // const ctx = renderer.getContext('2d');
-    //   const pixelArt = new PixelArtDrawer(ctx);
-    //   const points = [{
-    //     x: 0,
-    //     y: 0,
-    //   }, {
-    //     x: 0,
-    //     y: 0,
-    //   }];
+      const startDrawing = (e) => {
+        isMouseDown = true;
+        [startX, startY] = [e.offsetX, e.offsetY];
+      };
 
-    //   renderer.onmousemove = function (e) {
-    //     const rect = this.getBoundingClientRect();
-    //     const lastPoint = points[points.length - 1];
-    //     lastPoint.x = e.clientX - rect.left;
-    //     lastPoint.y = e.clientY - rect.top;
-    //   };
-    //   renderer.onclick = () => {
-    //     const lastPoint = points[points.length - 1];
-    //     points.push({
-    //       x: lastPoint.x,
-    //       y: lastPoint.y,
-    //     });
-    //   };
+      const stopDrawing = (e) => {
+        isMouseDown = false;
 
-    //   function drawLine(pt) {
-    //     pixelArt.lineTo(pt.x, pt.y);
-    //   }
-
-    //   function draw() {
-    //     ctx.clearRect(0, 0, renderer.width, renderer.height);
-    //     pixelArt.beginPath();
-    //     points.forEach(drawLine);
-    //     pixelArt.stroke();
-    //     requestAnimationFrame(draw);
-    //   }
-    //   draw();
-    // };
+      }
+    };
 
     const getActionButtons = (e) => {
       let target = e.target;
@@ -125,6 +91,9 @@ export default class Tools {
               console.log('pen-tool');
               // goPen();
               getPen();
+              break;
+            case 'pen-tool-test':
+              console.log('pen-tool-test');
               break;
             case 'bucket-tool':
               console.log('bucket-tool');
@@ -150,141 +119,3 @@ export default class Tools {
     canvasTools.addEventListener('click', getActionButtons);
   }
 }
-
-// class PixelArtDrawer {
-//   constructor(ctx, options = {}) {
-//     if (!(ctx instanceof CanvasRenderingContext2D)) {
-//       throw new TypeError('Invalid Argument 1, not a canvas 2d context');
-//     }
-//     this.cursor = {
-//       x: 0,
-//       y: 0,
-//     };
-//     this.strokeStyle = '#000';
-//     this.renderer = ctx;
-//     this.ctx = document.createElement('canvas').getContext('2d');
-//     this.setPixelSize((options && options.pixelSize) || 10);
-//   }
-
-//   setPixelSize(pixelSize) {
-//     this.pixelSize = pixelSize;
-//     const ctx = this.ctx;
-//     const canvas = ctx.canvas;
-//     const renderer = this.renderer.canvas;
-
-//     canvas.width = (renderer.width / pixelSize) || 0;
-//     canvas.height = (renderer.height / pixelSize) || 0;
-//     ctx.globalCompositeOperation = 'source-in';
-//     this.image = ctx.createImageData(canvas.width, canvas.height);
-//     this.data = new Uint32Array(this.image.data.buffer);
-//   }
-
-//   beginPath() {
-//     this.data.fill(0);
-//     this.cursor.x = this.cursor.y = null;
-//   }
-
-//   stroke() {
-//     const renderer = this.renderer;
-//     const currentSmoothing = renderer.imageSmoothingEnbaled;
-//     const ctx = this.ctx;
-//     ctx.putImageData(this.image, 0, 0);
-//     // put the color
-//     ctx.fillStyle = this.strokeStyle;
-//     ctx.fillRect(0, 0, this.image.width, this.image.height);
-//     renderer.imageSmoothingEnabled = false;
-//     renderer.drawImage(ctx.canvas, 0, 0, renderer.canvas.width, renderer.canvas.height);
-//     renderer.imageSmoothingEnabled = currentSmoothing;
-//   }
-
-//   moveTo(x, y) {
-//     this.cursor.x = (x / this.pixelSize) || 0;
-//     this.cursor.y = (y / this.pixelSize) || 0;
-//   }
-
-//   lineTo(x, y) {
-//     if (this.cursor.x === null) {
-//       this.moveTo(x, y);
-//       return;
-//     }
-//     const data = this.data;
-//     const width = this.image.width;
-//     const height = this.image.height;
-//     let x1 = this.cursor.x;
-//     let y1 = this.cursor.y;
-
-//     const x2 = (x / this.pixelSize) || 0;
-//     const y2 = (y / this.pixelSize) || 0;
-//     // from here it is OP's code
-//     const dx = Math.abs(x2 - x1);
-//     const sx = x1 < x2 ? 1 : -1;
-//     const dy = -Math.abs(y2 - y1);
-//     const sy = y1 < y2 ? 1 : -1;
-//     let e2;
-//     let er = dx + dy;
-//     let end = false;
-//     let index;
-//     while (!end) {
-//       // this check would probably be better done out of the loop
-//       if (x1 >= 0 && x1 <= width && y1 >= 0 && y1 <= height) {
-//         // here we need to convert x, y coords to array index
-//         index = ((y1 * width) + x1) || 0;
-//         data[index] = 0xff000000;
-//       }
-//       if (x1 === x2 && y1 === y2) {
-//         end = true;
-//       } else {
-//         e2 = 2 * er;
-//         if (e2 > dy) {
-//           er += dy;
-//           x1 += sx;
-//         }
-//         if (e2 < dx) {
-//           er += dx;
-//           y1 += sy;
-//         }
-//       }
-//     }
-//     this.cursor.x = x2;
-//     this.cursor.y = y2;
-//   }
-// }
-
-// const goPen = () => {
-//   // const ctx = renderer.getContext('2d');
-//   const pixelArt = new PixelArtDrawer(ctx);
-//   const points = [{
-//     x: 0,
-//     y: 0,
-//   }, {
-//     x: 0,
-//     y: 0,
-//   }];
-
-//   renderer.onmousemove = function (e) {
-//     const rect = this.getBoundingClientRect();
-//     const lastPoint = points[points.length - 1];
-//     lastPoint.x = e.clientX - rect.left;
-//     lastPoint.y = e.clientY - rect.top;
-//   };
-//   renderer.onclick = () => {
-//     const lastPoint = points[points.length - 1];
-//     points.push({
-//       x: lastPoint.x,
-//       y: lastPoint.y,
-//     });
-//   };
-
-//   function drawLine(pt) {
-//     pixelArt.lineTo(pt.x, pt.y);
-//   }
-
-//   function draw() {
-//     ctx.clearRect(0, 0, renderer.width, renderer.height);
-//     pixelArt.beginPath();
-//     points.forEach(drawLine);
-//     pixelArt.stroke();
-//     requestAnimationFrame(draw);
-//   }
-//   draw();
-// }
