7690e29087f8a8e55ca3f62b5a2422ef34f96be9
greatorangejuice <bypavelsnigirev@gmail.com>
2019-06-12 15:43:41 +0300

    feat: add clearfield tool

--- piskel-clone/changelog.md
+++ piskel-clone/changelog.md
@@ -4,6 +4,6 @@
 |-----------|-------------|-------------|-------------|
 | May 27 | May 30 | 20h | base structure of the project |
 | May 30 | Jun 2 | 15h | animation player |
-| Jun 3 | Jun 8 | 10h | code refactor and add new structure |
-| Jun 11 | Jun 12 | 5h | layers implementation |
+| Jun 3 | Jun 5 | 10h | code refactor and add new structure |
+| Jun 6 | Jun 6 | 3h | new pen implementation |
 | Jun 12 | Jun 12 | 10h | bug fixing in canvas implementation  
--- piskel-clone/src/screens/canvas/tools/toolsbar.js
+++ piskel-clone/src/screens/canvas/tools/toolsbar.js
@@ -1,48 +1,18 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable prefer-destructuring */
 export default class Tools {
+  constructor() {
+    const canvas = document.querySelector('.paint-field');
+    this.currentPaintFieldWidth = canvas.width;
+    console.log(canvas.width);
+    // const primaryColor = document.querySelector('.primary');
+  }
+
   setToolsbar() {
     const canvasTools = document.querySelector('.canvas-tools');
     const canvas = document.querySelector('.paint-field');
     const context = canvas.getContext('2d');
-    // const ctx = canvas.getContext('2d');
-    // const frame = new GetInitialCanvas();
-
-    // const getPen = () => {
-    //   context.lineCap = 'square';
-
-    //   let x = 0;
-    //   let y = 0;
-    //   let isMouseDown = false;
-
-    //   const stopDrawing = () => {
-    //     isMouseDown = false;
-    //   };
-    //   const startDrawing = (event) => {
-    //     isMouseDown = true;
-    //     [x, y] = [event.offsetX, event.offsetY];
-    //   };
-    //   const drawLine = (event) => {
-    //     if (isMouseDown) {
-    //       const newX = event.offsetX;
-    //       const newY = event.offsetY;
-    //       context.beginPath();
-    //       context.moveTo(x, y); // * 10 = 3d tube
-    //       // context.lineTo(newX, newY);
-    //       // context.fillRect(newX, newY, 10, 10);
-
-    //       // console.log(`newX: ${newX}, ceilX: ${Math.ceil(newX / 10) * 10}`);
-    //       context.fillRect(Math.ceil(newX / 4) * 4, Math.ceil(newY / 4) * 4, 4, 4);
-
-    //       context.stroke();
-    //       [x, y] = [newX, newY];
-    //     }
-    //   };
-
-    //   canvas.addEventListener('mousedown', startDrawing);
-    //   canvas.addEventListener('mousemove', drawLine);
-    //   canvas.addEventListener('mouseup', stopDrawing);
-    //   canvas.addEventListener('mouseout', stopDrawing);
-    // };
+    context.strokeStyle = 'red';
 
     const getTriangle = () => {
       console.log('triangle-tool');
@@ -57,10 +27,16 @@ export default class Tools {
       console.log('paintBucket');
     };
     const clearField = () => {
-      // context.clearRect(0, 0, canvas.width, canvas.height);
-      const mycanvas = document.querySelector('.canvas');
-      const width = mycanvas.offsetWidth;
-      console.log(width);
+      const activeFrame = document.querySelector('.active-frame');
+      const frameContext = activeFrame.getContext('2d');
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      frameContext.clearRect(0, 0, activeFrame.width, activeFrame.height);
+    };
+
+    const drawPixel = (x, y) => {
+      const primaryColor = document.querySelector('.primary');
+      context.fillStyle = primaryColor.value;
+      context.fillRect(Math.ceil(x / 4) * 4, Math.ceil(y / 4) * 4, 4, 4);
     };
 
     const brethPen = () => {
@@ -75,21 +51,14 @@ export default class Tools {
       const startDrawing = (e) => {
         isMouseDown = true;
         [x1, y1] = [e.offsetX, e.offsetY];
-        console.log(`x1: ${x1}, y1: ${y1}`);
       };
 
       const stopDrawing = () => {
         isMouseDown = false;
       };
 
-      const drawPixel = (x, y) => {
-        context.fillRect(Math.ceil(x / 4) * 4, Math.ceil(y / 4) * 4, 4, 4);
-      };
-
       const drawLine = (e) => {
-        console.log(isMouseDown);
         if (!isMouseDown) return;
-        console.log('drawline');
         [x2, y2] = [e.offsetX, e.offsetY];
         deltaX = Math.abs(x2 - x1);
         deltaY = Math.abs(y2 - y1);
@@ -116,6 +85,51 @@ export default class Tools {
       canvas.addEventListener('mousedown', startDrawing);
       canvas.addEventListener('mousemove', drawLine);
       canvas.addEventListener('mouseup', stopDrawing);
+      // canvas.addEventListener('mouseout', stopDrawing);
+    };
+
+    const circle = () => {
+      let x1 = 0;
+      let y1 = 0;
+      let isMouseDown = false;
+      let R = 0;
+      let x2 = 0;
+      let y2 = 0;
+      let delta = 1 - 2 * R;
+      let error = 0;
+
+      const startDrawing = (e) => {
+        isMouseDown = true;
+        [x1, y1] = [e.offsetX, e.offsetY];
+      };
+
+      const stopDrawing = () => {
+        isMouseDown = false;
+      };
+
+      const drawCircle = (e) => {
+        if (!isMouseDown) return;
+        [x2, y2] = [e.offsetX, e.offsetY];
+        R = x2 - x1;
+        delta = 1 - 2 * R;
+        while (y2 >= 0) {
+          drawPixel(x1 + 2, y1 + y2);
+          drawPixel(x1 + 2, y1 - y2);
+          drawPixel(x1 - 2, y1 + y2);
+          drawPixel(x1 - 2, y1 - y2);
+          error = 2 * (delta + y2) - 1;
+          while ((delta < 0) && (error <= 0)) {
+            delta += 2 * ++x2 + 1;
+          }
+          while ((delta > 0) && (error > 0)) {
+            delta -= 2 * --y2 + 1;
+          }
+          delta += 2 * (++x2 - y2--);
+        }
+      };
+      canvas.addEventListener('mousedown', startDrawing);
+      canvas.addEventListener('mousemove', drawCircle);
+      canvas.addEventListener('mouseup', stopDrawing);
       canvas.addEventListener('mouseout', stopDrawing);
     };
 
@@ -127,14 +141,12 @@ export default class Tools {
           switch (action) {
             case 'pen-tool':
               console.log('pen-tool');
-              // goPen();
-              // getPen();
               brethPen();
               break;
-            // case 'pen-tool-test':
-            //   console.log('pen-tool-test');
-            //   brethPen();
-            //   break;
+            case 'pen-tool-test':
+              console.log('circle');
+              circle();
+              break;
             case 'bucket-tool':
               console.log('bucket-tool');
               paintBucket();
@@ -146,6 +158,7 @@ export default class Tools {
               changeSize();
               break;
             case 'trash-tool':
+              console.log('clearField');
               clearField();
               break;
             default:
@@ -159,3 +172,43 @@ export default class Tools {
     canvasTools.addEventListener('click', getActionButtons);
   }
 }
+
+// const ctx = canvas.getContext('2d');
+// const frame = new GetInitialCanvas();
+
+// const getPen = () => {
+//   context.lineCap = 'square';
+
+//   let x = 0;
+//   let y = 0;
+//   let isMouseDown = false;
+
+//   const stopDrawing = () => {
+//     isMouseDown = false;
+//   };
+//   const startDrawing = (event) => {
+//     isMouseDown = true;
+//     [x, y] = [event.offsetX, event.offsetY];
+//   };
+//   const drawLine = (event) => {
+//     if (isMouseDown) {
+//       const newX = event.offsetX;
+//       const newY = event.offsetY;
+//       context.beginPath();
+//       context.moveTo(x, y); // * 10 = 3d tube
+//       // context.lineTo(newX, newY);
+//       // context.fillRect(newX, newY, 10, 10);
+
+//       // console.log(`newX: ${newX}, ceilX: ${Math.ceil(newX / 10) * 10}`);
+//       context.fillRect(Math.ceil(newX / 4) * 4, Math.ceil(newY / 4) * 4, 4, 4);
+
+//       context.stroke();
+//       [x, y] = [newX, newY];
+//     }
+//   };
+
+//   canvas.addEventListener('mousedown', startDrawing);
+//   canvas.addEventListener('mousemove', drawLine);
+//   canvas.addEventListener('mouseup', stopDrawing);
+//   canvas.addEventListener('mouseout', stopDrawing);
+// };
