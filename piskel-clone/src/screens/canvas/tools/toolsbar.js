e29770797790f7a849bc0bfa650ce6ab393f45e5
greatorangejuice <bypavelsnigirev@gmail.com>
2019-06-17 14:06:55 +0300

    feat: add Eraser

--- piskel-clone/src/screens/canvas/tools/toolsbar.js
+++ piskel-clone/src/screens/canvas/tools/toolsbar.js
@@ -32,7 +32,9 @@ export default class Tools {
     const clearCurrentState = () => {
       Object.keys(currentToolsListeners).forEach((key) => {
         canvas.removeEventListener(key, currentToolsListeners[key]);
-        Object.keys(currentToolsListeners).forEach((value) => { delete currentToolsListeners[value]; });
+        Object.keys(currentToolsListeners).forEach((value) => {
+          delete currentToolsListeners[value];
+        });
       });
       // Добавить удаление класса active или что-нибудь подобного.
     };
@@ -40,9 +42,10 @@ export default class Tools {
     const getTriangle = () => {
       console.log('triangle-tool');
     };
-    const paintBucket = () => {
-      console.log('paintBucket');
-    };
+    // const paintBucket = () => {
+    //   const test = context.getImageData(0, 0, 128, 128);
+    //   console.log(test.data);
+    // };
     const clearField = () => {
       const activeFrame = document.querySelector('.active-frame');
       const frameContext = activeFrame.getContext('2d');
@@ -50,14 +53,22 @@ export default class Tools {
       frameContext.clearRect(0, 0, activeFrame.width, activeFrame.height);
     };
 
-    const drawPixel = (x, y) => {
+    const drawPixel = (x, y, eraser = 0) => {
       const primaryColor = document.querySelector('.primary');
       context.fillStyle = primaryColor.value;
-      context.fillRect(Math.ceil(x / this.pixelWidth) * this.pixelWidth,
-        Math.ceil(y / this.pixelWidth) * this.pixelWidth, this.pixelWidth, this.pixelWidth);
+      if (eraser === 0) {
+        context.fillRect(Math.ceil(x / this.pixelWidth) * this.pixelWidth,
+          Math.ceil(y / this.pixelWidth) * this.pixelWidth, this.pixelWidth, this.pixelWidth);
+        console.log('drawing');
+      } else if (eraser === 1) {
+        context.fillStyle = 'red';
+        context.clearRect(Math.ceil(x / this.pixelWidth) * this.pixelWidth,
+          Math.ceil(y / this.pixelWidth) * this.pixelWidth, this.pixelWidth, this.pixelWidth);
+        console.log('erasing');
+      }
     };
 
-    const brethPen = () => {
+    const brethPen = (eraser = 0) => {
       let x1 = 0;
       let y1 = 0;
       let x2 = 0;
@@ -83,10 +94,10 @@ export default class Tools {
         const signX = x1 < x2 ? 1 : -1;
         const signY = y1 < y2 ? 1 : -1;
         let error = deltaX - deltaY;
-        drawPixel(x2, y2);
+        drawPixel(x2, y2, eraser);
 
         while (x1 !== x2 || y1 !== y2) {
-          drawPixel(x1, y1);
+          drawPixel(x1, y1, eraser);
           const error2 = error * 2;
 
           if (error2 > -deltaY) {
@@ -111,50 +122,50 @@ export default class Tools {
       addFunctionsInState('mouseout', stopDrawing);
     };
 
-    const circle = () => {
-      let x1 = 0;
-      let y1 = 0;
-      let isMouseDown = false;
-      let R = 0;
-      let x2 = 0;
-      let y2 = 0;
-      let delta = 1 - 2 * R;
-      let error = 0;
+    // const circle = () => {
+    //   let x1 = 0;
+    //   let y1 = 0;
+    //   let isMouseDown = false;
+    //   let R = 0;
+    //   let x2 = 0;
+    //   let y2 = 0;
+    //   let delta = 1 - 2 * R;
+    //   let error = 0;
 
-      const startDrawing = (e) => {
-        isMouseDown = true;
-        [x1, y1] = [e.offsetX, e.offsetY];
-      };
+    //   const startDrawing = (e) => {
+    //     isMouseDown = true;
+    //     [x1, y1] = [e.offsetX, e.offsetY];
+    //   };
 
-      const stopDrawing = () => {
-        isMouseDown = false;
-      };
+    //   const stopDrawing = () => {
+    //     isMouseDown = false;
+    //   };
 
-      const drawCircle = (e) => {
-        if (!isMouseDown) return;
-        [x2, y2] = [e.offsetX, e.offsetY];
-        R = x2 - x1;
-        delta = 1 - 2 * R;
-        while (y2 >= 0) {
-          drawPixel(x1 + 2, y1 + y2);
-          drawPixel(x1 + 2, y1 - y2);
-          drawPixel(x1 - 2, y1 + y2);
-          drawPixel(x1 - 2, y1 - y2);
-          error = 2 * (delta + y2) - 1;
-          while ((delta < 0) && (error <= 0)) {
-            delta += 2 * ++x2 + 1;
-          }
-          while ((delta > 0) && (error > 0)) {
-            delta -= 2 * --y2 + 1;
-          }
-          delta += 2 * (++x2 - y2--);
-        }
-      };
-      canvas.addEventListener('mousedown', startDrawing);
-      canvas.addEventListener('mousemove', drawCircle);
-      canvas.addEventListener('mouseup', stopDrawing);
-      canvas.addEventListener('mouseout', stopDrawing);
-    };
+    //   const drawCircle = (e) => {
+    //     if (!isMouseDown) return;
+    //     [x2, y2] = [e.offsetX, e.offsetY];
+    //     R = x2 - x1;
+    //     delta = 1 - 2 * R;
+    //     while (y2 >= 0) {
+    //       drawPixel(x1 + 2, y1 + y2);
+    //       drawPixel(x1 + 2, y1 - y2);
+    //       drawPixel(x1 - 2, y1 + y2);
+    //       drawPixel(x1 - 2, y1 - y2);
+    //       error = 2 * (delta + y2) - 1;
+    //       while ((delta < 0) && (error <= 0)) {
+    //         delta += 2 * ++x2 + 1;
+    //       }
+    //       while ((delta > 0) && (error > 0)) {
+    //         delta -= 2 * --y2 + 1;
+    //       }
+    //       delta += 2 * (++x2 - y2--);
+    //     }
+    //   };
+    //   canvas.addEventListener('mousedown', startDrawing);
+    //   canvas.addEventListener('mousemove', drawCircle);
+    //   canvas.addEventListener('mouseup', stopDrawing);
+    //   canvas.addEventListener('mouseout', stopDrawing);
+    // };
 
     const swapColor = () => {
       const primaryColor = document.querySelector('.primary');
@@ -163,6 +174,75 @@ export default class Tools {
       primaryColor.value = secondaryColor.value;
       secondaryColor.value = temp;
     };
+
+    // const testBucket = (e) => {
+    //   const colorLayer = context.getImageData(0, 0, canvas.width, canvas.height);
+
+    //   const matchStartColor = (pixelPos) => {
+    //     const r = colorLayer.data[pixelPos];
+    //     const g = colorLayer.data[pixelPos + 1];
+    //     const b = colorLayer.data[pixelPos + 2];
+
+    //     return (r === startR && g === startG && b === startB);
+    //   };
+
+    //   const colorPixel = (pixelPos) => {
+    //     colorLayer.data[pixelPos] = fillColorR;
+    //     colorLayer.data[pixelPos + 1] = fillColorG;
+    //     colorLayer.data[pixelPos + 2] = fillColorB;
+    //     colorLayer.data[pixelPos + 3] = 255;
+    //   };
+
+    //   const pixelStack = [
+    //     [e.offsetX, e.offsetY],
+    //   ];
+
+    //   while (pixelStack.length) {
+    //     let pixelPos;
+    //     let reachLeft;
+    //     let reachRight;
+    //     const newPos = pixelStack.pop();
+    //     const x = newPos[0];
+    //     let y = newPos[1];
+
+    //     pixelPos = (y * canvas.width + x) * 4;
+    //     while (y-- >= drawingBoundTop && matchStartColor(pixelPos)) {
+    //       pixelPos -= canvas.width * 4;
+    //     }
+    //     pixelPos += canvas.width * 4;
+    //     ++y;
+    //     reachLeft = false;
+    //     reachRight = false;
+    //     while (y++ < canvas.height - 1 && matchStartColor(pixelPos)) {
+    //       colorPixel(pixelPos);
+
+    //       if (x > 0) {
+    //         if (matchStartColor(pixelPos - 4)) {
+    //           if (!reachLeft) {
+    //             pixelStack.push([x - 1, y]);
+    //             reachLeft = true;
+    //           }
+    //         } else if (reachLeft) {
+    //           reachLeft = false;
+    //         }
+    //       }
+
+    //       if (x < canvas.width - 1) {
+    //         if (matchStartColor(pixelPos + 4)) {
+    //           if (!reachRight) {
+    //             pixelStack.push([x + 1, y]);
+    //             reachRight = true;
+    //           }
+    //         } else if (reachRight) {
+    //           reachRight = false;
+    //         }
+    //       }
+
+    //       pixelPos += canvas.width * 4;
+    //     }
+    //   }
+    //   context.putImageData(colorLayer, 0, 0);
+    // };
 
     const getActionButtons = (e) => {
       const target = e.target;
@@ -176,11 +256,14 @@ export default class Tools {
               break;
             case 'pen-tool-test':
               console.log('circle');
-              circle();
+              // circle();
+              clearCurrentState();
+              brethPen(1);
               break;
             case 'bucket-tool':
               console.log('bucket-tool');
-              paintBucket();
+              // paintBucket();
+              // testBucket();
               break;
             case 'triangle-tool':
               getTriangle();
